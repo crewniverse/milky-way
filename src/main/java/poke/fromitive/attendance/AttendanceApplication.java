4aab447364b6be1c05a833fd524dbd0852839602
fromitive <fromitive@gmail.com>
2024-06-18 16:51:58 +0900

    init commit

---
+++ src/main/java/poke/fromitive/attendance/AttendanceApplication.java
@@ -0,0 +1,13 @@
+package poke.fromitive.attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/poke/fromitive/attendance/AttendanceApplicationTests.java
@@ -0,0 +1,13 @@
+package poke.fromitive.attendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
