c933e0113547b52a76d7aa9650e72c6b2bb63476
fromitive <fromitive@gmail.com>
2024-06-22 20:16:54 +0900

    refactor: 타임아웃 예외처리 추가
    refactor: 불필요한 의존성 제거

--- src/main/java/poke/fromitive/attendance/service/AttendanceService.java
+++ src/main/java/poke/fromitive/attendance/service/AttendanceService.java
@@ -1,8 +1,9 @@
 package poke.fromitive.attendance.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import poke.fromitive.attendance.config.SheetOption;
 import poke.fromitive.attendance.domain.Attendance;
 import poke.fromitive.attendance.domain.Crew;
 import poke.fromitive.attendance.httpclient.AttendanceCrawler;
@@ -11,21 +12,20 @@ import poke.fromitive.attendance.response.AttendanceResponse;
 import poke.fromitive.attendance.response.AttendanceSheetResponses;
 
 import java.io.IOException;
+import java.net.SocketTimeoutException;
 import java.security.GeneralSecurityException;
 import java.time.LocalDate;
 import java.util.List;
 
 @Service
 public class AttendanceService {
+    private static final Logger log = LoggerFactory.getLogger(AttendanceService.class);
     private final AttendanceRepository attendanceRepository;
-    private final SheetOption sheetOption;
     private final AttendanceCrawler attendanceCrawler;
 
     public AttendanceService(final AttendanceRepository attendanceRepository,
-                             final SheetOption sheetOption,
                              final AttendanceCrawler attendanceCrawler) {
         this.attendanceRepository = attendanceRepository;
-        this.sheetOption = sheetOption;
         this.attendanceCrawler = attendanceCrawler;
     }
 
@@ -38,8 +38,12 @@ public class AttendanceService {
 
     @Transactional
     public void updateAttendance() throws GeneralSecurityException, IOException {
-        final AttendanceSheetResponses attendanceSheetResponses = attendanceCrawler.execute();
-        updateAttendance(attendanceSheetResponses.findByDate(LocalDate.now()));
+        try {
+            final AttendanceSheetResponses attendanceSheetResponses = attendanceCrawler.execute();
+            updateAttendance(attendanceSheetResponses.findByDate(LocalDate.now()));
+        } catch (SocketTimeoutException e) {
+            log.error("허걱! 출석부 불러오다가 타임아웃 걸렸구만유");
+        }
     }
 
     private void updateAttendance(final AttendanceSheetResponses attendanceSheetResponses) {
