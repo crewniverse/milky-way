7372b003ce6a851a80aa9cef1d3301c6d0edf66d
fromitive <fromitive@gmail.com>
2024-06-24 10:20:04 +0900

    style: 컨밴션 적용

--- src/test/java/poke/fromitive/attendance/controller/StaticPageControllerTest.java
+++ src/test/java/poke/fromitive/attendance/controller/StaticPageControllerTest.java
@@ -4,11 +4,11 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class StaticPageControllerTest extends AbstractControllerTest{
+class StaticPageControllerTest extends AbstractControllerTest {
 
     @DisplayName("메인 페이지를 정상적으로 불러온다.")
     @Test
-    void should_response_index_page(){
+    void should_response_index_page() {
         RestAssured.given().log().all()
                 .when().get("/")
                 .then().log().all()
