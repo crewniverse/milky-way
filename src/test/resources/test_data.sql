29efd03bf93d38286afd49737db12541b5982d1a
fromitive <fromitive@gmail.com>
2024-07-03 16:31:05 +0900

    test: sql 파일 수정

--- src/test/resources/test_data.sql
+++ src/test/resources/test_data.sql
@@ -1,7 +1,8 @@
-INSERT INTO crew (name, campus_name) VALUES ('포케', '잠실');
-INSERT INTO crew (name, campus_name) VALUES ('아루', '잠실');
-INSERT INTO crew (name, campus_name) VALUES ('무송', '선릉');
-INSERT INTO crew (name, campus_name) VALUES ('동훈', '선릉');
-INSERT INTO crew (name, campus_name) VALUES ('혁수', '선릉');
-INSERT INTO attendance (crew_id, date) VALUES (1, CURRENT_DATE());
-INSERT INTO attendance (crew_id, date) VALUES (2, CURRENT_DATE());
+INSERT INTO crew (name, campus) VALUES ('포케', 'JAMSIL')
+                                     ,('아루', 'JAMSIL')
+                                     ,('무송', 'SEOLLEUNG')
+                                     ,('동훈', 'SEOLLEUNG')
+                                     ,('혁수', 'SEOLLEUNG');
+INSERT INTO attendance (crew_id, date) VALUES (1, CURRENT_DATE()),
+                                              (2, CURRENT_DATE());
+
