0475e39b963335cc2145968af6576153b52a710d
fromitive <fromitive@gmail.com>
2024-07-05 23:54:28 +0900

    style: trailing commas 개선

--- src/test/resources/test_data.sql
+++ src/test/resources/test_data.sql
@@ -1,8 +1,7 @@
-INSERT INTO crew (name, campus) VALUES ('포케', 'JAMSIL')
-                                     ,('아루', 'JAMSIL')
-                                     ,('무송', 'SEOLLEUNG')
-                                     ,('동훈', 'SEOLLEUNG')
-                                     ,('혁수', 'SEOLLEUNG');
+INSERT INTO crew (name, campus) VALUES ('포케', 'JAMSIL'),
+                                       ('아루', 'JAMSIL'),
+                                    ('무송', 'SEOLLEUNG'),
+                                    ('동훈', 'SEOLLEUNG'),
+                                    ('혁수', 'SEOLLEUNG');
 INSERT INTO attendance (crew_id, date) VALUES (1, CURRENT_DATE()),
                                               (2, CURRENT_DATE());
-
